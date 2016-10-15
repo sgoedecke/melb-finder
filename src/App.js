c1d42fe0350387b927b2439782b6a6f974912754
Sean Goedecke <seangoedecke@gmail.com>
2016-10-15 17:21:34 +1100

    save local

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import $ from 'jquery';
 import _ from 'lodash';
 import logo from './logo.svg';
+import { getDistance } from 'geolib';
 import './App.css';
 
 const App = () => (
@@ -14,30 +15,33 @@ class Finder extends React.Component{
   constructor(props) {
     super();
     this.state = {
-      latitude: "calculating...",
-      longitude: "calculating...",
-      radius: "0.04",
+      latitude: "0",
+      longitude: "0",
+      radius: "0",
+      displayedRadius: "0",
       nearbyAttractions: [],
+      allAttractions: [],
       searching: false
       };
+  };
 
+  componentWillMount(){
+    this.search();
   };
 
   search = () => {
-    this.setState({searching: true});
+    this.setState({searching: true, allAttractions: [], nearbyAttractions: []});
     navigator.geolocation.getCurrentPosition(this.setLocation);
   };
 
-  setAttractions = (data) => {
-    console.log(data)
-    console.log(this.state.radius)
+  filterAttractions = () => {
     let nearbyAttractions = [];
-    data.forEach( (item) => {
+    this.state.allAttractions.forEach( (item) => {
       if (this.isNearby(this.state.radius, item, this.state.latitude, this.state.longitude)) {
         nearbyAttractions.push(item);
       }
     });
-    this.setState({searching: false, nearbyAttractions: nearbyAttractions});
+    this.setState({nearbyAttractions: nearbyAttractions});
   };
 
   isNearby = (radius, item, latitude, longitude) => {
@@ -46,36 +50,61 @@ class Finder extends React.Component{
 
   findAttractions = () => {
     console.log("Making API call...")
-    $.getJSON(this.props.API_ENDPOINT).then(this.setAttractions)
+    $.getJSON(this.props.API_ENDPOINT).then((data) => {this.setState({allAttractions: data})})
+    .then(this.filterAttractions)
+    .then(this.setState({searching: false})).then(console.log("Finished API call."));
   };
 
   setLocation = (position) => {
     this.setState({latitude: position.coords.latitude, longitude: position.coords.longitude});
-    navigator.geolocation.getCurrentPosition(this.findAttractions);
     this.findAttractions();
   };
+
+  calculateDisplayedRadius = () => {
+    const radius = getDistance(
+        {latitude: this.state.latitude,
+        longitude: this.state.longitude},
+        {latitude: parseFloat(this.state.latitude) + parseFloat(this.state.radius),
+        longitude: parseFloat(this.state.longitude) + parseFloat(this.state.radius)});
+    this.setState({displayedRadius: radius});
+  }
 
   render() {
     return (
       <div>
-        <h1>ATTRACTION FINDER</h1>
+        <div className="bg-primary header-box">
+          <h1>Spyglass</h1>
+        </div>
 
+        <div className= "bg-info app-box">
+          <input type='range' min='0' max='0.09' defaultValue ='0' step='0.0001' onChange={ (e) => {
+            this.setState({radius: e.target.value});
+            this.calculateDisplayedRadius();
+            this.filterAttractions();
+          } } />
+          <p>Search radius: { this.state.displayedRadius }m (approx.)</p>
 
-        <h2>Look how far out?</h2>
-        <input type='text' defaultValue ='0.04' onChange={ (e) => {
-          console.log(e.target.value)
-          this.setState({radius: e.target.value});
-        } } />
+          {
+            (this.state.allAttractions.length == 0) &&
+            <div>
+              <h2>Searching...</h2>
+            </div>
+          }
+          {
+            (this.state.allAttractions.length > 0) &&
+            <div>
+              <button onClick={this.search} disabled={this.state.searching}> Search again... </button>
+              <h2>Showing {this.state.nearbyAttractions.length} ({this.state.allAttractions.length} total):</h2>
+            </div>
+          }
+          {
+            this.state.nearbyAttractions.map( (item) => {
+            return(
+              <Location key={_.uniqueId()} userLatitude={this.state.latitude} userLongitude={this.state.longitude} {...item} />
+            )
+          }) }
 
-        <button onClick={this.search} disabled={this.state.searching}> Search! </button>
-
-        <h2>{this.state.nearbyAttractions.length} found:</h2>
-        {
-          this.state.nearbyAttractions.map( (item) => {
-          return(
-            <Location key={_.uniqueId()} {...item} />
-          )
-        }) }
+        </div>
 
     </div>)
   };
@@ -97,21 +126,30 @@ class Location extends React.Component {
   render() {
     const props = this.props;
     return(
-      <div className='item-box' onClick={this.toggleDisplay}>
-        <h2>{props.name}</h2>
-        <h3>Artist: {props.artist}</h3>
-        {
-        this.state.expanded &&
-        <div>
-        <p>Description: {props.structure_}</p>
-        <p>Address: {props.addresspt}</p>
-        <p>Date: {props.artdate}</p>
-        </div>
-        }
-        {!this.state.expanded &&
-          <p>...</p>
-        }
+      <div className='item-wrapper'>
+        <div className='item-box' onClick={this.toggleDisplay}>
+          <h2>{props.name}</h2>
+          <h3>Artist: {props.artist}</h3>
 
+          <p>Distance: {getDistance(
+            {latitude: props.userLatitude,
+            longitude: props.userLongitude},
+            {latitude: props.geom.latitude,
+            longitude: props.geom.longitude})} meters</p>
+
+          {
+          this.state.expanded &&
+          <div>
+          <p>Description: {props.structure_}</p>
+          <p>Address: {props.addresspt}</p>
+          <p>Date: {props.artdate}</p>
+          </div>
+          }
+          {!this.state.expanded &&
+            <p>...</p>
+          }
+
+        </div>
       </div>
   )};
 }
